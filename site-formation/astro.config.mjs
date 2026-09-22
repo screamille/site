55dba7234a3a448af9b0ca5bcc2c5bf4a61f76ba
screamille <screamille@gmail.com>
2026-09-22 22:04:34 +0200

    Delete site-formation directory

--- site-formation/astro.config.mjs
+++
@@ -1,6 +0,0 @@
-import { defineConfig } from "astro/config";
-
-export default defineConfig({
-  site: "https://www.exemple.fr", // remplace par ton domaine (même valeur que SITE.url dans src/config.ts)
-  build: { inlineStylesheets: "always" },
-});
